bb0e81a483b83c197159a553bb842c3ef07debec
LuciaLanganey <langaneylucia03@gmail.com>
2026-03-31 17:50:13 -0700

    added ...

--- src/pages/LuciaLanganey.tsx
+++ src/pages/LuciaLanganey.tsx
@@ -1,3 +1,11 @@
+import myPhoto from "../assets/lucialanganey.jpeg";
+
 export default function LuciaLanganey() {
-  return <div>Lucia Langaney</div>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-[24pt] font-bold">Lucia Langaney</h1>
+      <img src={myPhoto} alt="Lucia" className="w-[100px] h-[100px]" />
+      <a href="https://github.com/lucialanganey">Github</a>
+    </div>
+  );
 }
